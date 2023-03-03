9f14051ce4a893dac5a1176f4dad35302d837197
kushagra-unorg <kushagra.agnihotri@unorg.in>
2023-03-03 11:55:58 +0530

    added ColorTypes

---
+++ src/components/types/types.ts
@@ -0,0 +1,14 @@
+export type ColorsType =
+  | "white"
+  | "green"
+  | "black"
+  | "grey"
+  | "purple"
+  | "light-purple"
+  | "dark-purple"
+  | "orange"
+  | "light-orange"
+  | "delivered"
+  | "not-delivered"
+  | "partial"
+  | "pending";
