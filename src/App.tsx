a800d2d44bdf4daeeae732143ce98e0282f46cb0
kushagra-unorg <kushagra.agnihotri@unorg.in>
2023-03-17 12:02:37 +0530

    tested modals

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,8 @@ import { Button } from "./components/Buttons";
 import { ModalConfigType } from "./components/types/modalTypes";
 import useModal from "./components/useModal";
 
-function TestModalBody() {
-  return <h1>Modal Test Hello</h1>;
-}
-function TestModal2Body() {
-  return <h1>Modal2 Test Hello</h1>;
+function TestModalBody({ title }: { title: string }) {
+  return <h1>Modal {title}</h1>;
 }
 
 const modalConfig: ModalConfigType[] = [
@@ -16,13 +13,25 @@ const modalConfig: ModalConfigType[] = [
     name: "test",
     title: "test",
     type: "right",
-    children: <TestModalBody />,
+    children: <TestModalBody title="test" />,
   },
   {
     name: "test2",
     title: "test2",
     type: "bottom",
-    children: <TestModal2Body />,
+    children: <TestModalBody title="test2" />,
+  },
+  {
+    name: "test3",
+    title: "test3",
+    type: "right",
+    children: <TestModalBody title="test3" />,
+  },
+  {
+    name: "test4",
+    title: "test4",
+    type: "bottom",
+    children: <TestModalBody title="test4" />,
   },
 ];
 
@@ -38,6 +47,8 @@ function App() {
       {Modal}
       <Button clickEvent={() => openModal("test")}>Click</Button>
       <Button clickEvent={() => openModal("test2")}>Click2</Button>
+      <Button clickEvent={() => openModal("test3")}>Click3</Button>
+      <Button clickEvent={() => openModal("test4")}>Click4</Button>
     </div>
   );
 }
